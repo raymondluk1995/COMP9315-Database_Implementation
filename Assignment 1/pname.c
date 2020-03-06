d413b5822a4dbb99e7b61bdedb33ef79018d27a9
z5277884 <z5277884@cse.unsw.edu.au>
2020-03-06 11:22:29 +1100

    start redoing

--- Assignment 1/pname.c
+++ Assignment 1/pname.c
@@ -1,3 +1,5 @@
+
+
 /*
  * src/tutorial/complex.c
  *
@@ -7,164 +9,203 @@
   format for these routines is dictated by Postgres architecture.
 ******************************************************************************/
 
-#include "fmgr.h"
-#include "libpq/pqformat.h" /* needed for send/recv functions */
 #include "postgres.h"
 
-#define COMMASTRING ","
-#define SPACE ' '
-#define HYPHEN '-'
-#define QUOTE '\''
-#define FAMILY 0
-#define GIVEN 1
+#include "fmgr.h"
+#include "libpq/pqformat.h"		/* needed for send/recv functions */
 
 PG_MODULE_MAGIC;
 
-// created by Vigo
-typedef struct PersonName 
+typedef struct Complex
 {
-  char *family;
-  char *given
-} PersonName;
+	double		x;
+	double		y;
+}			Complex;
 
-/*****************************************************************************
- * functions used for defined
- *****************************************************************************/
-
-// check the validity of family or given name
-bool checkComponentVaild(char *component, unsigned int length, bool isgiven) 
-{
-  bool upperFlag = true;
-  int word_len = 0;
-
-  if (component[0] == SPACE) 
-  {
-    if (!isgiven)
-      return false;
-    else {
-      if (!isupper(component[1]))
-        return false;
-    }
-  }
-
-  for (int i = 0; i < length; i++)
-  {
-    if (component[i] == SPACE || component[i] == HYPHEN ||
-        component[i] == QUOTE) {
-      // the char with index[i] is space, - or ' next char should be upper letter
-      if (i == length - 1 && !isupper(component[i + 1]))
-        return false;
-      else {
-        upperFlag = true;
-        if (component[i] == SPACE && word_len < 2 && i != 0)
-          return false;
-        word_len = (component[i] == SPACE) ? 0 : word_len;
-      }
-    } 
-    else if (isupper(component[i]))
-    {
-      if (upperFlag == false)
-        return false;
-      // in case of consecutive upper letter
-      upperFlag = false;
-
-    } 
-    else if (!isalpha(component[i]))
-      return false;
-    word_len++;
-  }
-  return true;
-}
-
-// trim the left space of given name
-char *ltrim(char *str) {
-  if (str == NULL || *str == '\0')
-    return str;
-  int len = 0;
-  char *c = str;
-  while (*c != '\0' && isspace(*c))
-  {
-    ++c;
-    ++len;
-  }
-  memmove(str, c, strlen(str) - len + 1);
-  return str;
-}
-
-// report error function
-void errorReport(char *type, char *words)
-{
-  ereport(ERROR,
-          (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-           errmsg("invalid input syntax for type %s: \"%s\"", type, words)));
-}
 
 /*****************************************************************************
  * Input/Output functions
  *****************************************************************************/
 
-PG_FUNCTION_INFO_V1(pname_in);
+PG_FUNCTION_INFO_V1(complex_in);
 
-Datum pname_in(PG_FUNCTION_ARGS)
+Datum
+complex_in(PG_FUNCTION_ARGS)
 {
-  // the whole input text
-  char *str = PG_GETARG_CSTRING(0);
-  unsigned int len = strlen(str);
-  char new[len];
-  strcpy(new, str);
+	char	   *str = PG_GETARG_CSTRING(0);
+	double		x,
+				y;
+	Complex    *result;
 
-  // used for storing family and given name
-  char *family;
-  char *given;
+	if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+				 errmsg("invalid input syntax for type %s: \"%s\"",
+						"complex", str)));
 
-  PersonName *result;
-
-  // memory allocation
-  result = (PersonName *)palloc(sizeof(PersonName));
-
-  // extraction of family and given name
-  family = strtok(new, COMMASTRING);
-  given = strtok(NULL, COMMASTRING);
-
-  if (strtok(NULL, COMMASTRING) != NULL || (given == NULL) ||
-      (family == NULL))
-    // error report
-    errorReport("PersonName", str);
-
-  if (!checkComponentVaild(family, strlen(family), FAMILY) ||
-      !checkComponentVaild(given, strlen(given), GIVEN)) 
-  {
-    // error report
-    errorReport("PersonName", str);
-  }
-
-  // process the given name
-  given = ltrim(given);
-
-  // memory allocation
-  result->family = malloc(sizeof(char) * strlen(family));
-  result->given = malloc(sizeof(char) * strlen(given));
-
-  // stored it into result
-  strcpy(result->family, family);
-  strcpy(result->given, given);
-
-  PG_RETURN_POINTER(result);
+	result = (Complex *) palloc(sizeof(Complex));
+	result->x = x;
+	result->y = y;
+	PG_RETURN_POINTER(result);
 }
 
-PG_FUNCTION_INFO_V1(pname_out);
+PG_FUNCTION_INFO_V1(complex_out);
 
-Datum pname_out(PG_FUNCTION_ARGS) 
+Datum
+complex_out(PG_FUNCTION_ARGS)
 {
-  PersonName *pname = (PersonName *)PG_GETARG_POINTER(0);
-  char *result;
+	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
+	char	   *result;
 
-  result = psprintf("%s,%s", pname->family, pname->given);
-  PG_RETURN_CSTRING(result);
+	result = psprintf("(%g,%g)", complex->x, complex->y);
+	PG_RETURN_CSTRING(result);
 }
 
-// there may be a single space after the comma
-// there will be no people with just one name (e.g. no Prince, Jesus, Aristotle,
-// etc.) there will be no numbers (e.g. noGates, William III) there will be no
-// titles (e.g. no Dr, Prof, Mr, Ms) there will be no initials (e.g. no
-// Shepherd,John A)
+/*****************************************************************************
+ * Binary Input/Output functions
+ *
+ * These are optional.
+ *****************************************************************************/
+
+PG_FUNCTION_INFO_V1(complex_recv);
+
+Datum
+complex_recv(PG_FUNCTION_ARGS)
+{
+	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
+	Complex    *result;
+
+	result = (Complex *) palloc(sizeof(Complex));
+	result->x = pq_getmsgfloat8(buf);
+	result->y = pq_getmsgfloat8(buf);
+	PG_RETURN_POINTER(result);
+}
+
+PG_FUNCTION_INFO_V1(complex_send);
+
+Datum
+complex_send(PG_FUNCTION_ARGS)
+{
+	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
+	StringInfoData buf;
+
+	pq_begintypsend(&buf);
+	pq_sendfloat8(&buf, complex->x);
+	pq_sendfloat8(&buf, complex->y);
+	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+}
+
+/*****************************************************************************
+ * New Operators
+ *
+ * A practical Complex datatype would provide much more than this, of course.
+ *****************************************************************************/
+
+PG_FUNCTION_INFO_V1(complex_add);
+
+Datum
+complex_add(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+	Complex    *result;
+
+	result = (Complex *) palloc(sizeof(Complex));
+	result->x = a->x + b->x;
+	result->y = a->y + b->y;
+	PG_RETURN_POINTER(result);
+}
+
+
+/*****************************************************************************
+ * Operator class for defining B-tree index
+ *
+ * It's essential that the comparison operators and support function for a
+ * B-tree index opclass always agree on the relative ordering of any two
+ * data values.  Experience has shown that it's depressingly easy to write
+ * unintentionally inconsistent functions.  One way to reduce the odds of
+ * making a mistake is to make all the functions simple wrappers around
+ * an internal three-way-comparison function, as we do here.
+ *****************************************************************************/
+
+#define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
+
+static int
+complex_abs_cmp_internal(Complex * a, Complex * b)
+{
+	double		amag = Mag(a),
+				bmag = Mag(b);
+
+	if (amag < bmag)
+		return -1;
+	if (amag > bmag)
+		return 1;
+	return 0;
+}
+
+
+PG_FUNCTION_INFO_V1(complex_abs_lt);
+
+Datum
+complex_abs_lt(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);
+}
+
+PG_FUNCTION_INFO_V1(complex_abs_le);
+
+Datum
+complex_abs_le(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);
+}
+
+PG_FUNCTION_INFO_V1(complex_abs_eq);
+
+Datum
+complex_abs_eq(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);
+}
+
+PG_FUNCTION_INFO_V1(complex_abs_ge);
+
+Datum
+complex_abs_ge(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);
+}
+
+PG_FUNCTION_INFO_V1(complex_abs_gt);
+
+Datum
+complex_abs_gt(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);
+}
+
+PG_FUNCTION_INFO_V1(complex_abs_cmp);
+
+Datum
+complex_abs_cmp(PG_FUNCTION_ARGS)
+{
+	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+
+	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
+}
